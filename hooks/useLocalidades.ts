47b2b91e734af3ff0d0d846c456a0572562998c7
Nicolas Londinsky <london@MacBookAir.localdomain>
2026-06-09 10:46:30 -0300

    localidades hardcodeadas sin API externa

--- hooks/useLocalidades.ts
+++ hooks/useLocalidades.ts
@@ -1,36 +1,34 @@
-import { useState, useEffect } from "react";
+export const PROVINCIAS_AR = [
+  "Buenos Aires", "CABA", "Catamarca", "Chaco", "Chubut",
+  "Cordoba", "Corrientes", "Entre Rios", "Formosa", "Jujuy",
+  "La Pampa", "La Rioja", "Mendoza", "Misiones", "Neuquen",
+  "Rio Negro", "Salta", "San Juan", "San Luis", "Santa Cruz",
+  "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucuman"
+];
 
-export interface Provincia {
-  id: string;
-  nombre: string;
-}
-
-export interface Municipio {
-  id: string;
-  nombre: string;
-}
-
-export function useProvincias() {
-  const [provincias, setProvincias] = useState<Provincia[]>([]);
-
-  useEffect(() => {
-    fetch("https://apis.datos.gob.ar/georef/api/provincias?orden=nombre&max=100")
-      .then(res => res.json())
-      .then(data => setProvincias(data.provincias || []));
-  }, []);
-
-  return { provincias };
-}
-
-export function useMunicipios(provinciaNombre: string) {
-  const [municipios, setMunicipios] = useState<Municipio[]>([]);
-
-  useEffect(() => {
-    if (!provinciaNombre) { setMunicipios([]); return; }
-    fetch("https://apis.datos.gob.ar/georef/api/municipios?provincia=" + encodeURIComponent(provinciaNombre) + "&orden=nombre&max=500")
-      .then(res => res.json())
-      .then(data => setMunicipios(data.municipios || []));
-  }, [provinciaNombre]);
-
-  return { municipios };
-}
+export const CIUDADES_POR_PROVINCIA: { [key: string]: string[] } = {
+  "Buenos Aires": ["La Plata", "Mar del Plata", "Quilmes", "Lomas de Zamora", "Lanus", "General San Martin", "Tres de Febrero", "Moreno", "Florencio Varela", "Berazategui", "Almirante Brown", "Tigre", "San Isidro", "Vicente Lopez", "Avellaneda", "Merlo", "Moron", "Hurlingham", "Ituzaingo", "San Miguel", "Malvinas Argentinas", "Jose C. Paz", "Esteban Echeverria", "Ezeiza", "Presidente Peron", "San Vicente", "Pilar", "Escobar", "Zarate", "Campana", "San Nicolas", "Pergamino", "Junin", "Tandil", "Bahia Blanca", "Necochea", "Olavarria", "Azul", "Chivilcoy", "Mercedes", "San Pedro", "Lujan", "Lobos", "Chascomus", "Dolores", "Pinamar", "Villa Gesell", "Miramar", "Coronel Suarez", "Tres Arroyos"],
+  "CABA": ["Palermo", "Belgrano", "Recoleta", "Caballito", "Flores", "Villa Urquiza", "Almagro", "Balvanera", "San Telmo", "La Boca", "Barracas", "Boedo", "Colegiales", "Coghlan", "Devoto", "Villa del Parque", "Villa Pueyrredon", "Paternal", "Chacarita", "Villa Crespo", "Nunez", "Saavedra", "Parque Patricios", "Nueva Pompeya", "Villa Lugano", "Mataderos", "Liniers", "Versalles", "Monte Castro", "Villa Real", "Floresta", "Velez Sarsfield", "Villa Luro", "Villa General Mitre", "Agronomia", "Villa Ortuzar", "Parque Chas", "Villa Santa Rita"],
+  "Cordoba": ["Cordoba Capital", "Villa Carlos Paz", "Rio Cuarto", "San Francisco", "Villa Maria", "Alta Gracia", "La Falda", "Cosquin", "Bell Ville", "Jesus Maria", "Morteros", "Laboulaye", "Rio Tercero", "Arroyito", "Marcos Juarez", "Cruz del Eje", "Dean Funes"],
+  "Santa Fe": ["Rosario", "Santa Fe Capital", "Rafaela", "Venado Tuerto", "Reconquista", "Santo Tome", "Villa Constitucion", "Galvez", "Casilda", "Esperanza", "Las Rosas", "Ceres", "San Jorge"],
+  "Mendoza": ["Mendoza Capital", "Godoy Cruz", "Guaymallen", "Las Heras", "Lujan de Cuyo", "Maipu", "San Rafael", "General Alvear", "Malargue", "Rivadavia", "Junin", "San Martin"],
+  "Tucuman": ["San Miguel de Tucuman", "Tafi Viejo", "Yerba Buena", "Banda del Rio Sali", "Aguilares", "Concepcion", "Famailla", "Monteros"],
+  "Salta": ["Salta Capital", "San Ramon de la Nueva Oran", "Tartagal", "Rosario de la Frontera", "Cafayate", "Embarcacion", "Joaquin V. Gonzalez"],
+  "Entre Rios": ["Parana", "Concordia", "Gualeguaychu", "Concepcion del Uruguay", "Colon", "Victoria", "Gualeguay", "La Paz"],
+  "Neuquen": ["Neuquen Capital", "Plottier", "Cipolletti", "Cutral Co", "Plaza Huincul", "Zapala", "San Martin de los Andes", "Villa La Angostura"],
+  "Rio Negro": ["Viedma", "Bariloche", "Cipolletti", "Allen", "General Roca", "El Bolson", "Cinco Saltos"],
+  "Chaco": ["Resistencia", "Presidencia Roque Saenz Pena", "Villa Angela", "Charata", "Barranqueras"],
+  "Corrientes": ["Corrientes Capital", "Goya", "Posadas", "Curuzú Cuatia", "Mercedes", "Santo Tome", "Paso de los Libres"],
+  "Misiones": ["Posadas", "Oberá", "Eldorado", "Leandro N. Alem", "Puerto Iguazu", "Apostoles"],
+  "Jujuy": ["San Salvador de Jujuy", "Palpalá", "San Pedro de Jujuy", "Libertador General San Martin", "Humahuaca"],
+  "San Juan": ["San Juan Capital", "Rivadavia", "Rawson", "Chimbas", "Santa Lucia", "Caucete"],
+  "San Luis": ["San Luis Capital", "Villa Mercedes", "Merlo", "Quines"],
+  "La Pampa": ["Santa Rosa", "General Pico", "Toay", "Eduardo Castex"],
+  "Catamarca": ["San Fernando del Valle de Catamarca", "Andalgala", "Tinogasta", "Belen"],
+  "La Rioja": ["La Rioja Capital", "Chilecito", "Aimogasta"],
+  "Formosa": ["Formosa Capital", "Clorinda", "Pirané", "El Colorado"],
+  "Chubut": ["Rawson", "Comodoro Rivadavia", "Trelew", "Puerto Madryn", "Esquel", "Rada Tilly"],
+  "Santa Cruz": ["Rio Gallegos", "Caleta Olivia", "Pico Truncado", "Puerto Deseado", "El Calafate"],
+  "Santiago del Estero": ["Santiago del Estero Capital", "La Banda", "Termas de Rio Hondo", "Frías", "Añatuya"],
+  "Tierra del Fuego": ["Ushuaia", "Rio Grande", "Tolhuin"],
+};
